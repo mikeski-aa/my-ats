d44e4606e3913c93ba0a89e44c5f35850d954a70
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-04 15:27:21 +0100

    Fixed export issue

--- src/components/CSVDownload.tsx
+++ src/components/CSVDownload.tsx
@@ -5,6 +5,8 @@ import { CSVLink } from "react-csv";
 function DownloadCSV({ apps }: { apps: IStagedata[] }) {
   const csvData = Papa.unparse(apps);
 
+  console.log(apps);
+
   return (
     <div className="csvDownloadHolder">
       <CSVLink data={csvData} filename="data.csv">
