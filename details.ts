db74cb47a057e40d66dc347f51780987b6d14e69
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-07 14:41:39 +0100

    odd fix

---
+++ details.ts
@@ -0,0 +1,11 @@
+import { IDetails } from "./src/interface";
+
+const details: IDetails = {
+  email: "asdasdas@gmail.com",
+  number: "+49 123123123",
+  portfolio: "https:/",
+  linkedin: "https:///",
+  github: "https://github.com/",
+};
+
+export { details };
