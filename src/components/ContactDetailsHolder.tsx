fd301b60f3798c865eee18999b06c99d3c1c9a36
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-07 14:47:11 +0100

    Changed

--- src/components/ContactDetailsHolder.tsx
+++ src/components/ContactDetailsHolder.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { details } from "../../data.ts";
+import { details } from "../../details.ts";
 import "../styles/contactDetails.css";
 import DetailsInput from "./DetailsInput.tsx";
 import { IDetails } from "../interface.ts";
